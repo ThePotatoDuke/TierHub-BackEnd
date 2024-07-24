ba6014ea186dbb1a5d11d418078d663ea53d1094
Mustafa <gameacedamy@gmail.com>
2024-07-24 15:10:44 +0300

    deletion and image upload fixed fr now

--- src/main/java/com/example/TierHub/config/WebConfig.java
+++ src/main/java/com/example/TierHub/config/WebConfig.java
@@ -19,6 +19,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
-                .addResourceLocations("classpath:/static/images/");
+                .addResourceLocations("file:src/main/resources/static/images/");
     }
+
 }
